d9080c3d2f8e8c6c297abd51566f3052f999ec74
shaheenshay <shaheen.shay@gmail.com>
2024-10-09 17:03:53 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main(String a[]) 
+    {
+        int num1 = 13;
+        int num2 = 24;
+        int result = num1 +num2;
+        System.out.println(result);
+    }
+}
---
+++ Statements.java
@@ -0,0 +1,11 @@
+public class Statements {
+    public static void main(String a[]) {
+        int i = 1;
+        
+        while (i<=555) {
+            System.out.println("Hi" + i);
+
+            i++;
+        }
+    }
+}
